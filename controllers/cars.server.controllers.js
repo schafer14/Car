757b06bea374b0bf1b465431b5c74126c94ea6a7
Banner <banner.schafer@gmail.com>
2014-07-20 17:57:09 +0800

    populate function

--- controllers/cars.server.controllers.js
+++ controllers/cars.server.controllers.js
@@ -130,49 +130,49 @@ exports.populate = function() {
 
 		_.forEach(cars, function(car) {
 			setTimeout(function() {
-				
-				console.log(car.name);
-				var url = 'http://www.gumtree.com.au' + car.url;
-				var reqOptions = {
-					url: url,
-				    headers: {
-				        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36',
-				    }
-				};
-				request(reqOptions, function(err, response, body) {
-					if (err) throw err;
+				console.log('thing');
+				// console.log(car.name);
+				// var url = 'http://www.gumtree.com.au' + car.url;
+				// var reqOptions = {
+				// 	url: url,
+				//     headers: {
+				//         'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36',
+				//     }
+				// };
+				// request(reqOptions, function(err, response, body) {
+				// 	if (err) throw err;
 					
-					var temp = {};
-					var $ = cheerio.load(body);
-					var listed = $('div#ad-attributes dl').each(function(index, attribute) {
-						var title = $(this).find('dt').html().trim().slice(0, -1);
-						var attr = $(this).find('dd').html().trim();
-						if (map[title]) {
-							if (map[title] === 'make') {
-								if ($(this).find('dd').find('a').html()) {
-									car.make = $(this).find('dd').find('a').html().trim();
-								}
-							} else if (map[title] === 'model') {
-								if ($(this).find('dd').find('a').html()) {
-									car.model = $(this).find('dd').find('a').html().trim();
-								}
-							} else if (map[title] === 'year') {
-								if ($(this).find('dd').find('a').html()) {
-									car.year = $(this).find('dd').find('a').html().trim();
-								}
-							} else if (map[title] === 'registered') {
-								car.registered = attr === 'Yes' ? true : false;
-							} else {
-								car[map[title]] = attr;
-							}
-						};
-						car.complete = true;
-						car.save(function(err) {
-							if (err) throw err;
-							console.log(car);
-						});
-					});
-				});
+				// 	var temp = {};
+				// 	var $ = cheerio.load(body);
+				// 	var listed = $('div#ad-attributes dl').each(function(index, attribute) {
+				// 		var title = $(this).find('dt').html().trim().slice(0, -1);
+				// 		var attr = $(this).find('dd').html().trim();
+				// 		if (map[title]) {
+				// 			if (map[title] === 'make') {
+				// 				if ($(this).find('dd').find('a').html()) {
+				// 					car.make = $(this).find('dd').find('a').html().trim();
+				// 				}
+				// 			} else if (map[title] === 'model') {
+				// 				if ($(this).find('dd').find('a').html()) {
+				// 					car.model = $(this).find('dd').find('a').html().trim();
+				// 				}
+				// 			} else if (map[title] === 'year') {
+				// 				if ($(this).find('dd').find('a').html()) {
+				// 					car.year = $(this).find('dd').find('a').html().trim();
+				// 				}
+				// 			} else if (map[title] === 'registered') {
+				// 				car.registered = attr === 'Yes' ? true : false;
+				// 			} else {
+				// 				car[map[title]] = attr;
+				// 			}
+				// 		};
+				// 		car.complete = true;
+				// 		car.save(function(err) {
+				// 			if (err) throw err;
+				// 			console.log(car);
+				// 		});
+				// 	});
+				// });
 			}, Math.random() * (7000 - 3000) + 3000);
 
 		});
